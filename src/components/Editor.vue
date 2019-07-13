674e1dd591250da37d20539edc2f5eadf4a00a2c
Elliot Forbes <emforce@live.com>
2019-07-13 17:29:38 +0100

    Added a simple textarea

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-        <h1 class="text-lg">GO Challenge - Getting Started</h1>
-        <div>
-            
-        </div>
+  <div>
+    <h1 class="text-lg">GO Challenge - Getting Started</h1>
+    <div class="box border rounded flex flex-col shadow bg-white">
+      <div class="box__title bg-grey-lighter px-3 py-2 border-b">
+        <h3 class="text-sm text-grey-darker font-medium">Source</h3>
+      </div>
+      <textarea class="text-grey-darkest flex-1 p-2 m-1 bg-transparent" name="tt" rows="20">package main
+
+import "fmt"
+
+func main() {
+    // do something here
+}
+      </textarea>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Editor'
-}
+  name: "Editor"
+};
 </script>
 
